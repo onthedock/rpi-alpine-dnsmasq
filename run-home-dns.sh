8d2c20174f0289434f50704a6ca2cee0fa6c54ad
Xavi Aznar <x.inthecloud+onthedock@gmail.com>
2017-08-27 20:40:28 +0000

    Nombre del contenedor en una variable
    Cambiamos el nombre del contenedor por una variable en el script
    de inicio del contenedor.

--- run-home-dns.sh
+++ run-home-dns.sh
@@ -1,9 +1,10 @@
-docker stop home-dns
-docker rm home-dns
-docker run -d --name home-dns \
+CONTAINER_NAME="home-dns"
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker run -d --name $CONTAINER_NAME \
 	-p 53:53 -p 53:53/udp \
 	--cap-add=NET_ADMIN \
-	-v $(PWD)/dnsmasq.conf:/etc/dnsmasq.conf \
-	-v $(PWD)/hosts:/etc/hosts \
+	-v $(pwd)/dnsmasq.conf:/etc/dnsmasq.conf \
+	-v $(pwd)/hosts:/etc/hosts \
 	xaviaznar/rpi-dnsmasq
 
