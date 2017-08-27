726935ad1a8b1c295762bca737b8aa469c87d5d2
On The Dock <x.inthecloud+onthedock@gmail.com>
2017-08-27 22:35:28 +0200

    Sustuidas rutas locales por $(PWD)
    Eliminadas las rutas locales de los ficheros montados en el contenedor.

--- run-home-dns.sh
+++ run-home-dns.sh
@@ -3,7 +3,7 @@ docker rm home-dns
 docker run -d --name home-dns \
 	-p 53:53 -p 53:53/udp \
 	--cap-add=NET_ADMIN \
-	-v /home/pirate/home-dns/dnsmasq.conf:/etc/dnsmasq.conf \
-	-v /home/pirate/home-dns/hosts:/etc/hosts \
+	-v $(PWD)/dnsmasq.conf:/etc/dnsmasq.conf \
+	-v $(PWD)/hosts:/etc/hosts \
 	xaviaznar/rpi-dnsmasq
 
